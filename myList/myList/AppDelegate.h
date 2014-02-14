b5ed81f54f0264f6f3f4d99839663d21ad64d45b
frankregel <frank.regel@gmail.com>
2014-02-14 08:54:31 +0100

    Initial Commit

---
+++ myList/myList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myList
+//
+//  Created by Frank Regel on 14.02.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myList/myList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myList
+//
+//  Created by Frank Regel on 14.02.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
